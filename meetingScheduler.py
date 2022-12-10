9d8d99c1b40929586bc2306983af6d1aa436f400
zvimosh <zvimoshko@gmail.com>
2022-12-10 19:59:51 +0200

    updated parseSchedules function

--- meetingScheduler.py
+++ meetingScheduler.py
@@ -2,11 +2,20 @@
 import re
 import json
 
+def find_value_by_key(key, value, list_of_dicts):
+    """Return True if value is found in list_of_dicts, False otherwise"""
+
+    #print(f"key: {key}, value: {value}, list_of_dicts: {list_of_dicts}")
+    if any(_dict[key] == value for _dict in list_of_dicts if key in _dict):
+        return True
+    else:
+        return False
+
 def getScheuldes():
     # Get the schedules from file
     # Return the schedules
     try:
-        with open('schedules.txt') as f:
+        with open('schedules1.txt') as f:
             schedules = f.readlines()
     except FileNotFoundError as e:
         print('ERROR:', e)
@@ -16,7 +25,8 @@ def getScheuldes():
 def parseSchedules(schedules):
     # Parse the schedules
     # Return the parsed schedules
-    parsedSchedules = {}
+    parsedSchedules = []
+    schedulesJson = {}
     for line in schedules:
         schedule = line.split()
         name = schedule[0][:-1]
@@ -26,23 +36,34 @@ def parseSchedules(schedules):
             location = "yard"
         else:  
             location = "" 
-        #print(line)    
-        print('"Name":"{0}", "Location"":"{1}", "Day":"{2}", "time":"{3}"'.format(name,location,day,time))
-        parsedSchedules.append('Name:{0}, Location:{1}, Day:{2}, time:{3}'.format(name,location,day,time))
+        #print(line)
+        dict_full = {'name':name, 'location':location, 'day':day, 'time':time}
+        dict = {'location':location, 'day':day, 'time':time}
+        #print('"Name":"{0}", "Location"":"{1}", "Day":"{2}", "time":"{3}"'.format(name,location,day,time))
+        parsedSchedules.append(dict_full)
+        if not schedulesJson:
+            schedulesJson[name] = [dict]
+        elif name in schedulesJson:
+            schedulesJson[name].append(dict)
+        else:
+            schedulesJson.update({name:[dict]})
 
-        #jsonObj = json.dumps(list)
-        #print(jsonObj)
-    ## need to work on serilizating to json or format that can work with
-    print(parsedSchedules)
-    jsonObj = json.dumps(parsedSchedules)
-    print(jsonObj)
 
-
-    return parsedSchedules
+    #print(schedulesJson)
+    return schedulesJson
 
 if __name__ == '__main__':
-    schedules = getScheuldes()
-    parsedSchedules = parseSchedules(schedules)
-    #print(schedules)
+    student = []
+    parsedSchedules = parseSchedules(schedules = getScheuldes())
+    
     #print(parsedSchedules)
+    
+"""     for item in parsedSchedules:
+        if find_value_by_key(key = 'name', value = item['name'], list_of_dicts = parsedSchedules):
+            student.append(item)
+        else:
+            student.append(item) """
+    #print(student)
+
+
 
